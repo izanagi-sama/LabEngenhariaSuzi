12903c288cb20b44f9a5deecb2edd56246b04643
rbran <433c8ad8@opayq.com>
2016-08-16 17:13:42 +0000

    criado um php de exemplo e script que faz update do server

---
+++ php/exemplo.php
@@ -0,0 +1,24 @@
+<?php
+
+
+header('Content-type: application/json; charset=utf-8');
+
+// converto o input em json; o "@" remove a mensagem de erro (caso existir)
+$input = @json_decode(file_get_contents("php://input"));
+
+//caso não receber o login e senha, retornar um erro
+if($input == null or !isset($input->login) or !isset($input->senha)) {
+    //envia resposta de erro
+    echo json_encode(['resposta' => 'erro', 'mensagem' => 'Requisição invalida']);
+    exit;
+}
+
+
+if($input->login != "admin" or $input->senha != "123qwe") {
+    //envia resposta de erro
+    echo json_encode(['resposta' => 'erro', 'mensagem' => 'Login ou Senha Invalido']); 
+    exit;
+}
+
+echo json_encode(['resposta' => 'sucesso']);
+exit;
