d78ce4ba09d247d4b22ea72ad94120f7cb21056c
rbran <433c8ad8@opayq.com>
2016-08-24 12:03:11 +0000

    Adicionado PHP de exmplo, que poder ser usados como guia pelo backend e de exemplo para testar o frontend

--- php/login/admin.php
+++ php/login/admin.php
@@ -1,6 +1,5 @@
 <?php
-
-
+//implementação para teste do frontend:
 header('Content-type: application/json; charset=utf-8');
 
 // converto o input em json; o "@" remove a mensagem de erro (caso existir)
@@ -9,15 +8,15 @@ $input = @json_decode(file_get_contents("php://input"));
 //caso não receber o login e senha, retornar um erro
 if($input == null or !isset($input->login) or !isset($input->senha)) {
     //envia resposta de erro
-    echo json_encode(['resposta' => 'erro', 'mensagem' => 'Requisição invalida']);
+    echo json_encode(['resultado' => false, 'mensagem' => 'Requisição invalida']);
     exit;
 }
 
 if($input->login != "admin" or $input->senha != "123qwe") {
     //envia resposta de erro
-    echo json_encode(['resposta' => 'erro', 'mensagem' => 'Login ou Senha Invalido']); 
+    echo json_encode(['resultado' => false, 'mensagem' => 'Login ou Senha Invalido']); 
     exit;
 }
 
-echo json_encode(['resposta' => 'sucesso']);
+echo json_encode(['resultado' => true, 'jwt' => 'asdasdasdasdsdgh']);
 exit;
