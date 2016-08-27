76d3bdffae66b1a9c1ec187ee0e3b86730ac7778
rbran <433c8ad8@opayq.com>
2016-08-27 12:22:43 +0000

    Adicionado tela de Cadastro Freelancer User Story #58, Task #75

--- html/js/login-freelancer.js
+++ html/js/login-freelancer.js
@@ -1,7 +1,37 @@
-app.controller("LoginFreelancerController", function($scope, $location, LoginService, store, jwtHelper) {
-    $scope.dataLoginFreelancer = {};
-    $scope.usuario = {};
+app.controller("LoginFreelancerController", function($scope, $location, store, jwtHelper, LoginService, FreelancerService) {
+    $scope.dataLogin = {
+        paginaCadastro: "views/cadastra-freelancer.html",
+        erro: null,
+        cadastrar: false,
+        usuarioCadastra: {},
+        usuarioLogin: {}
+    };
     
+    $scope.mostrarTelaCadastrar = function(ativa) {
+        if($scope.dataLogin.cadastrar == ativa) return;
+        $scope.dataLogin.erro = null;
+        $scope.dataLogin.cadastrar = ativa;
+    }
+    
+    $scope.cadastrar = function(usuario, senha2) {
+        if(senha2 != usuario.senha) {
+            $scope.dataLogin.erro = "Erro: Senhas são diferentes";
+            return;
+        }
+        $scope.dataMain.loading = true;
+        var resposta = FreelancerService.cadastra(usuario);
+            resposta.then(function(data) {
+            if(data.resultado == true) {
+                $scope.dataLogin.erro = null;
+                $scope.dataLogin.cadastrar = false;
+                $scope.dataMain.loading = false;
+            } else {
+                $scope.dataLogin.erro = "Erro no Cadastro: " + data.mensagem;
+                $scope.dataMain.loading = false;
+            }
+        });
+    };
+        
     $scope.login = function(usuario) {
         $scope.dataMain.loading = true;
         var resposta = LoginService.loginFreelancer(usuario.login, usuario.senha);
@@ -13,8 +43,8 @@ app.controller("LoginFreelancerController", function($scope, $location, LoginSer
                 $location.path('/freelancer');
                 $scope.dataMain.loading = false;
             } else {
+                $scope.dataLogin.erro = "Erro no login: " + data.mensagem;
                 $scope.dataMain.loading = false;
-                alert("Login Invalido");
             }
         });
     };
