e938692ac937780333a1df7e9cb71f785607ed0c
rbran <433c8ad8@opayq.com>
2016-08-30 22:07:08 +0000

    modificado pequenos detalhes na API

--- php/freelancer/retorna-dados.php
+++ php/freelancer/retorna-dados.php
@@ -1,6 +1,6 @@
 <?php
 //implementação para teste do frontend:
 header('Content-type: application/json; charset=utf-8');
-echo json_encode(['nome' => 'Freelancer1', 'email'=> 'email@example.com', 'cpf'=> '123123123', 'senha'=> 'asdasd', 'espcialidade'=> [2, 5, 7]]);
+echo json_encode(['nome' => 'Freelancer1', 'email'=> 'email@example.com', 'cpfcnpj'=> '12312312313', 'senha'=> 'asdasd', 'especialidade'=> ["2", "3", "7"]]);
 exit;
 
