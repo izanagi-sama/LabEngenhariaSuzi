2769215cca7eafd354f1664e356c86c0203edc5e
saxiro <fabriciopbrb@gmail.com>
2016-08-30 03:30:33 -0300

    CRUD pronto da tabela freelancer + arquivos
    Arquivos adicionais para teste de implementação

--- php/freelancer/cadastra.php
+++ php/freelancer/cadastra.php
@@ -1,5 +1,38 @@
 <?php
 //implementação para teste do frontend:
 header('Content-type: application/json; charset=utf-8');
-echo json_encode(['resultado' => true]);
+
+$freela = array();
+$dsn = 'mysql:dbname=mporn;host=localhost';
+$user = 'root';
+$password = '';
+
+if (isset($_POST['nome'])){
+    $freela['nome']  = $_POST['nome'];
+    $freela['email'] = $_POST['email'];
+    $freela['cpf']   = $_POST['cpf'];
+    $freela['senha'] = $_POST['senha'];
+} else {
+    echo json_encode(['resultado' => false]);
+    exit;
+}
+
+if($freela['nome'] == ''){echo json_encode(['resultado' => false]); exit;}
+if($freela['email'] == ''){echo json_encode(['resultado' => false]); exit;}
+
+try{
+    
+    $conn = new PDO($dsn, $user, $password);
+    
+    $query = "INSERT INTO freelancer (nome, email, cpf, senha) VALUES ('".$freela['nome']."','".$freela['email']."', '".$freela['cpf']."', '".$freela['senha']."')";
+    if($conn->exec($query)){echo json_encode(['resultado' => true]);}
+    
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+    
+    echo json_encode(['resultado' => false]);
+}
+
+$conn = null;
+
 exit;
