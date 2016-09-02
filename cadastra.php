cdee5afe2031fa7a846419040a4fdf6700efc8c4
saxiro <fabriciopbrb@gmail.com>
2016-09-02 07:01:20 -0300

    Cadastro sendo feito junto com especialidade
    Consegui fazer o cadastro junto com especialidade... pra ir tudo junto....

---
+++ cadastra.php
@@ -0,0 +1,66 @@
+<?php
+//implementação para teste do frontend:
+header('Content-type: application/json; charset=utf-8');
+
+require_once("../config.php");
+
+$freela = array();
+
+//$input = @json_decode(file_get_contents("php://input"));
+
+$especialidade[] = array(3, 6, 8);
+$input = array("nome" => "Juirana","email" => "juirana@gmail.com", "cpf" => "0344322345", "senha"=>"asdf","especialidade" => [3,6,8]);
+
+
+
+//TODO: verificar o JWT não somente o nome
+if (isset($input['nome'])){
+    $freela['nome']  = $input['nome'];
+    $freela['email'] = $input['email'];
+    $freela['cpf']   = $input['cpf'];
+    $freela['senha'] = $input['senha'];
+} else {
+    echo json_encode(['resultado' => false, 'mensagem' => 'Requisição invalida']);
+    exit;
+}
+
+if($freela['nome'] == ''){echo json_encode(['resultado' => false]); exit;}
+if($freela['email'] == ''){echo json_encode(['resultado' => false]); exit;}
+
+try{
+    
+    $pdo = new PDO($dsn, $user, $password);
+  
+    $sql = "INSERT INTO freelancer (nome, email, cpf, senha) VALUES (:nome,:email, :cpf, :senha)";
+    
+    $stmt = $pdo->prepare($sql);
+    $stmt->bindParam(':nome', $freela['nome']);
+    $stmt->bindParam(':email', $freela['email']);
+    $stmt->bindParam(':cpf', $freela['cpf']);
+    $stmt->bindParam(':senha', md5( $freela['senha'] ));
+    $stmt->execute();
+    
+    $id_freelancer = $pdo->lastInsertId();
+   
+    
+    foreach ($input['especialidade'] as $esp){
+     
+        $sql = "Insert into freelancer_especialidade values (:free,:esp)";
+        
+        $stmt = $pdo->prepare( $sql );
+        $stmt->bindParam(':free', $id_freelancer,PDO::PARAM_INT);
+        $stmt->bindParam(':esp', $esp,PDO::PARAM_INT);
+        $stmt->execute();
+    }
+    
+    echo json_encode(['resultado' => true]);
+    
+} catch (PDOException $e) {
+    //O retorno é todo em JSON, jamas faça isso:
+    //echo 'Connection failed: ' . $e->getMessage();
+    echo json_encode(['resultado' => false, 'mensagem' => 'Erro no Banco de Dados']);
+}
+
+$conn = null;
+
+exit;
