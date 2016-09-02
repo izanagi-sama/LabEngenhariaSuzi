86038c5c02292f1e7c22ec84c8e8a406d024a1b5
saxiro <fabriciopbrb@gmail.com>
2016-09-02 06:04:49 -0300

    Alterações feitas
    Alterações foram feitas. Fiz o teste recebendo em Json e está alterando corretamente o banco de dados.. obs: Não entendi ainda como que eu vou receber o id_freelancer pelo JWT... não consegui encontrar o exemplo que disseram que tem
    obs2: A tela que altera as especialidades tem que ser diferente... de repente uma em que a pessoa vai clicando pra acrescentar ou excluir  as especialidades....

--- php/freelancer/altera-dados.php
+++ php/freelancer/altera-dados.php
@@ -6,26 +6,49 @@ require_once("../config.php");
 
 $freela = array();
 
+//Teste
+//$input =   array("id_freelancer" => 1, "nome" => "Fabricio Mudado", "email"=>"fabriciopbrb@gmail.com", "cpf"=>"03939223913","senha"=>"asdf");  
+
 $input = @json_decode(file_get_contents("php://input"));
 
 //TODO: verificar o JWT
 if (isset($input['id_freelancer'])){
-    //$freela['id_freelancer'] = $input['id_freelancer']; //recebe via JWT
+    $freela['id_freelancer'] = $input['id_freelancer']; //recebe via JWT  ???? Procurei aqui nos arquivos e não descobri como pegar o id pelo JWT...
     $freela['nome']  = $input['nome'];
     $freela['email'] = $input['email'];
     $freela['cpf']   = $input['cpf'];
     $freela['senha'] = $input['senha'];
-    //TODO: faltou especialidade $input['especialidade--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------']
+    
+  
+    
+    /*Não dá pra usar especialidades na mesma tela que altera dados... as especialidades têm que ser acrescentadas em outra tela...
+     * Ou a mesma tela vai fazer uma chamada pra ir acrescentando ou deletando uma especialidade por vez....
+     */
+    
+    
 } else {
     echo json_encode(['resultado' => false, 'mensagem' => 'Requisição invalida']);
     exit;
 }
 
 try{
-    $conn = new PDO($dsn, $user, $password);
-    //TODO: use a API de subtituição de parametros, isso é MUITO errado
-    $query = "UPDATE freelancer SET nome = '".$freela['nome']."', email = '".$freela['email']."', cpf = '".$freela['cpf']."', senha = '".$freela['senha']."' WHERE id_freelancer = ".$freela['id_freelancer'];
-    if($conn->exec($query)){echo json_encode(['resultado' => true]);}
+    $pdo = new PDO($dsn, $user, $password);
+    $query = "UPDATE freelancer SET nome = :nome, email = :email, cpf = :cpf, senha = :senha WHERE id_freelancer = :id_freelancer";
+    
+    $stmt = $pdo->prepare( $query );
+    $stmt->bindParam(':nome',$freela['nome']);
+    $stmt->bindParam(':email', $freela['email']);
+    $stmt->bindParam(':cpf', $freela['cpf']);
+    $stmt->bindParam(':senha', $freela['senha']);
+    $stmt->bindParam(':id_freelancer', $freela['id_freelancer'],PDO::PARAM_INT);
+    
+    $result = $stmt->execute();
+    
+    if(!$result){
+        echo json_encode(['resultado' => false, 'mensagem' => 'Erro no Banco de Dados']);
+    } else {
+        echo json_encode(['resultado' => true]);
+    }
     
 } catch (PDOException $e) {
     //O retorno é todo em JSON, jamas faça isso:
@@ -33,6 +56,6 @@ try{
     echo json_encode(['resultado' => false, 'mensagem' => 'Erro no Banco de Dados']);
 }
 
-$conn = null;
+$pdo = null;
 
 exit;
