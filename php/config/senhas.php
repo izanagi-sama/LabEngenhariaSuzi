7a8a143dea7008b7e05c829490b91a429f3e4008
rbran <433c8ad8@opayq.com>
2016-09-06 21:59:07 +0000

    adicionado .gitignore, assim senhas não seram enviadas ao github

--- php/config/senhas.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('JWT_KEY', '');      //precisa ser adicionado manualmente
-define('BD_USER', '');      //precisa ser adicionado manualmente
-define('BD_PASSWORD', '');  //precisa ser adicionado manualmente
