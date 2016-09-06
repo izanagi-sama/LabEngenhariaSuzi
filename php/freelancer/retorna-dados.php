25043ccc1b5fbe9b961cf6b33921fb7459d00e55
saxiro <fabriciopbrb@gmail.com>
2016-09-06 01:59:58 -0300

    Update retorna-dados.php
    Nesse caso já estava pronto.. só fiz uns testes aqui e retirei alguns comentários...

--- php/freelancer/retorna-dados.php
+++ php/freelancer/retorna-dados.php
@@ -1,6 +1,38 @@
 <?php
+
 //implementação para teste do frontend:
 header('Content-type: application/json; charset=utf-8');
-echo json_encode(['nome' => 'Freelancer1', 'email'=> 'email@example.com', 'cpfcnpj'=> '12312312313', 'senha'=> 'asdasd', 'especialidade'=> ["2", "3", "7"]]);
-exit;
 
+
+use \Firebase\JWT\JWT;
+require_once("../vendor/autoload.php");
+
+require_once("../config.php");
+include("../recebe-jwt.php");
+
+
+
+$id = $token->data->id;
+
+
+try{
+    $pdo = new PDO($dsn, $user, $password);
+   
+    $stmt = $pdo->prepare('SELECT nome,email,cpf as cpfcnpj FROM freelancer WHERE id_freelancer = :id');
+    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+    
+    $stmt->execute();
+    
+    if($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+        echo json_encode($row);
+    } else {
+        echo json_encode(['resultado' => false]);
+    }
+    
+} catch (PDOException $e) {
+    echo json_encode(['resultado' => false, 'mensagem' => 'Connection failed: ' . $e->getMessage()]);
+}
+
+$conn = null;
+
+exit;
